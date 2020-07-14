f9dcfacaed471f4035a32fca7e9a8878e313bf37
wolfe00 <george.wolfe@softwire.com>
2020-07-14 14:21:00 +0100

    Resolve Red-07 Tests

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,7 +11,23 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var dxdy = new List<Tuple<int, int>>(){Tuple.Create(-1, -1), Tuple.Create(-1, 1), Tuple.Create(1, -1), Tuple.Create(1, 1), Tuple.Create(-1, 0), Tuple.Create(1, 0), Tuple.Create(0, -1), Tuple.Create(0, 1)};
+            var moves = new List<Square>();
+
+            foreach (var dir in dxdy)
+            {
+                var dx = dir.Item1;
+                var dy = dir.Item2;
+                var x = currentSquare.Row + dx;
+                var y = currentSquare.Col + dy;
+                if (board.InBounds(Square.At(x,y)))
+                {
+                    moves.Add(Square.At(x, y));
+                }
+            }
+
+            return moves;
         }
     }
 }
