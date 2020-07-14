2b1845fbc8c0d1313eedd4ac756a0b39a0e708ca
wolfe00 <george.wolfe@softwire.com>
2020-07-14 16:23:00 +0100

    Resolve Red-17 Tests (13-16 were already done before I got to them oops)

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -20,12 +21,31 @@ namespace Chessington.GameEngine.Pieces
             var ahead = Square.At(currentSquare.Row + shift, currentSquare.Col);
             var twoAhead = Square.At(currentSquare.Row + 2 * shift, currentSquare.Col);
             var moves = new List<Square>();
+            
             if (!board.HasPiece(ahead))
             {
                 moves.Add(ahead);
                 if (!HasMoved && !board.HasPiece(twoAhead))
                 {
                     moves.Add(twoAhead);
+                }
+            }
+            
+            var takedrowdcol = new List<Tuple<int, int>>()
+            {
+                Tuple.Create(shift, -1), 
+                Tuple.Create(shift, 1),
+            };
+
+            foreach (var direction in takedrowdcol)
+            {
+                var drow = direction.Item1;
+                var dcol = direction.Item2;
+                var row = currentSquare.Row + drow;
+                var col = currentSquare.Col + dcol;
+                if (board.HasEnemy(Square.At(row, col), this.Player))
+                {
+                    moves.Add(Square.At(row, col));
                 }
             }
             
